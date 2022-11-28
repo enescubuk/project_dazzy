9c2e7996d9f50d2c94bce4410483e3a6a0813e85
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-11-28 23:21:55 +0300

    Cube List Mini Game 3

---
+++ project_dazzy/Assets/Scripts/Mini Game/Mini Game 3/ButtonObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonObject : MonoBehaviour
+{
+    public UnityEvent unityEvent = new UnityEvent();
+    public GameObject button;
+
+    private void Start()
+    {
+        button = this.gameObject;
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray,out hit) && hit.collider.gameObject == gameObject)
+            {
+                unityEvent.Invoke();
+            }
+        }
+    }
+}
