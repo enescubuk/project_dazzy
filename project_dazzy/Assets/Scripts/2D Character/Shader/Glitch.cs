17eb7491a6f299921b7f3d6f57cb85787ec41f9f
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-12-22 02:04:43 +0300

    2dSpriteanim

---
+++ project_dazzy/Assets/Scripts/2D Character/Shader/Glitch.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kino;
+
+public class Glitch : MonoBehaviour
+{
+    public DigitalGlitch GlitchEffect;
+    public AnalogGlitch Glitchx;
+    
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Glitchx.colorDrift = 0.2f;
+        }
+
+        if (Glitchx.colorDrift > 0)
+        {
+            Glitchx.colorDrift -= 0.05f * Time.deltaTime;
+        }
+        else
+        {
+            Glitchx.colorDrift = 0;
+        }
+        */
+    }
+}
