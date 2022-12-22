8c301d41f270b641f8ee7b1489d3135f090df820
Kadir Cem Yılmaz <73888204+pholsen@users.noreply.github.com>
2022-12-22 15:32:35 +0300

    DOTween
    a

--- project_dazzy/Assets/Scripts/Objects/doorOpener.cs
+++ project_dazzy/Assets/Scripts/Objects/doorOpener.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class doorOpener : MonoBehaviour
 {
+    bool isOpen;
+    public GameObject door;
+
+    void Start()
+    {
+        DOTween.Init();
+
+        
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Cube" || other.tag == "Player")
         {
-            transform.GetChild(0).Translate(new Vector3(0,12,0));
+            
+            isOpen = true;
+            door.transform.DOMoveY(door.transform.position.y+20,0.5f);
+            transform.GetChild(1).Translate(new Vector3(0,-2,0));
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Cube" || other.tag  == "Player")
         {
-            transform.GetChild(0).Translate(new Vector3(0,-12,0));
+            
+            isOpen = false;
+            door.transform.DOMoveY(door.transform.position.y-20,0.5f);
+            //transform.GetChild(0).Translate(new Vector3(0,-12,0));
+            transform.GetChild(1).Translate(new Vector3(0,+2,0));
         }
     }
 }
