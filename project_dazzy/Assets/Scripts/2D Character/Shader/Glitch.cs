089cdb84970a6a6b60aed2e927d07deda89d61c0
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-12-22 04:34:28 +0300

    GlitchShader

--- project_dazzy/Assets/Scripts/2D Character/Shader/Glitch.cs
+++ project_dazzy/Assets/Scripts/2D Character/Shader/Glitch.cs
@@ -1,29 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Kino;
+using UnityEngine.PlayerLoop;
 
 public class Glitch : MonoBehaviour
 {
     public DigitalGlitch GlitchEffect;
     public AnalogGlitch Glitchx;
+    private bool EffectControl;
     
-    void Update()
+    void Start()
     {
-        /*
-        if (Input.GetKeyDown(KeyCode.Space))
+        Glitchx.scanLineJitter = 0.5f;
+        Glitchx.verticalJump = 0.11f;
+        Glitchx.colorDrift = 0.2f;
+        GlitchEffect.intensity = 0.1f;
+        StartCoroutine(GlitchEffectControl());
+    }
+    private void Update()
+    {
+        if (EffectControl)
         {
-            Glitchx.colorDrift = 0.2f;
+            Glitchx.colorDrift -= 0.015f * Time.deltaTime;
+            Glitchx.verticalJump -= 0.015f * Time.deltaTime;
+            Glitchx.scanLineJitter -= 0.015f * Time.deltaTime;
+            GlitchEffect.intensity -= 0.015f * Time.deltaTime;
         }
 
-        if (Glitchx.colorDrift > 0)
-        {
-            Glitchx.colorDrift -= 0.05f * Time.deltaTime;
-        }
-        else
+        if (Glitchx.colorDrift <= 0)
         {
             Glitchx.colorDrift = 0;
         }
-        */
+        if (Glitchx.verticalJump <= 0)
+        {
+            Glitchx.verticalJump = 0;
+        }if (Glitchx.scanLineJitter <= 0)
+        {
+            Glitchx.scanLineJitter = 0;
+        }if (GlitchEffect.intensity <= 0)
+        {
+            GlitchEffect.intensity = 0;
+        }
+    }
+
+    IEnumerator GlitchEffectControl()
+    {
+        yield return new WaitForSeconds(0.2f);
+        EffectControl = true;
     }
 }
