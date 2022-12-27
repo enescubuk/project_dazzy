9084b02ec71ed8150fdf371518e7b3443813090d
Kadir Cem Yılmaz <73888204+pholsen@users.noreply.github.com>
2022-12-27 21:39:58 +0300

    StateStart

---
+++ project_dazzy/Assets/Scripts/States/IdleState.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State
+{
+    // Start is called before the first frame update
+    public override State RunCurrentState()
+    {
+        return this;
+      /* // 
+
+        if ()
+        {
+            
+
+        }
+        else
+        {
+            
+        }
+        
+        Debug.Log("u falling");
+
+
+
+        //State Machine
+        if ()
+        {
+            
+            return unbalancedTier2State;
+        }
+        else if ()
+        {
+            return balancedState;
+        }
+        else
+        {
+            
+        }*/  }
+}
